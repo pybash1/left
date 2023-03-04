ad9cad1cad83cdaf5726c33e03f4ab21a2cea441
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-03-04 11:56:50 +0530

    style: setup and init basic icons, fonts, configs, etc

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: "#222222",
+        accent: "#ffffff",
+        neutral: "#888888",
+      },
+      fontFamily: {
+        mono: ["mono", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
