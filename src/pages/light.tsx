80ce43ae02947b0202bf0399a09b7f3a1d8edd9a
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-03-04 19:03:02 +0530

    fix(easter-eg): added an easter egg
    egg*

---
+++ src/pages/light.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { useEffect } from "react";
+
+const Light: NextPage = () => {
+  useEffect(() => {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  });
+
+  return <></>;
+};
+
+export default Light;
