8c05b041d1375080cbf064e88e5b4bd1c8aead8b
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-03-04 19:05:38 +0530

    feat(sidebar): added sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,37 @@
+const Sidebar = ({ hidden = false, headings, line }: Props) => {
+  return (
+    <div
+      className={`${
+        hidden ? "hidden" : ""
+      } fixed top-0 bottom-0 left-0 h-screen w-80 px-8 py-24 text-sm`}
+    >
+      <ul className="flex flex-col gap-1">
+        <li className="cursor-pointer font-bold">README.md</li>
+        {headings.map((heading, ind) => (
+          <li
+            className={`flex cursor-pointer flex-row items-center justify-between gap-8 ${
+              heading.line === line ? "text-accent" : "text-neutral"
+            }`}
+            key={ind}
+          >
+            <div>
+              {heading.heading.startsWith("# ")
+                ? heading.heading.replace("# ", "")
+                : "- " + heading.heading.replace("## ", "")}
+            </div>
+            <div>{heading.line}</div>
+          </li>
+        ))}
+        
+      </ul>
+    </div>
+  );
+};
+
+interface Props {
+  hidden?: boolean;
+  headings: { heading: string; line: number }[];
+  line: number;
+}
+
+export default Sidebar;
