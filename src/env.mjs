da9d26da2848d5cc28f08d07a2a022be4c91805a
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-03-06 18:26:48 +0530

    fix(env): configure env vars

--- src/env.mjs
+++ src/env.mjs
@@ -13,7 +13,7 @@ const server = z.object({
  * built with invalid env vars. To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
 const client = z.object({
-  // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+  NEXT_PUBLIC_NODE_ENV: z.enum(["dev", "prod"]),
 });
 
 /**
@@ -24,7 +24,7 @@ const client = z.object({
  */
 const processEnv = {
   NODE_ENV: process.env.NODE_ENV,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
 };
 
 // Don't touch the part below
