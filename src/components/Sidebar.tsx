6a65a3b080883168c82f3f293212199b06aec746
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-03-07 17:01:56 +0530

    feat(integration): now works with backend and is dynamic with new user support

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,14 @@
-const Sidebar = ({ hidden = false, headings, line, light }: Props) => {
+import { Dispatch, SetStateAction } from "react";
+
+const Sidebar = ({
+  hidden = false,
+  headings,
+  line,
+  light,
+  files,
+  current,
+  setCurrent,
+}: Props) => {
   return (
     <div
       className={`${
@@ -6,30 +16,79 @@ const Sidebar = ({ hidden = false, headings, line, light }: Props) => {
       } fixed top-0 bottom-0 left-0 h-screen w-80 px-8 py-24 text-sm`}
     >
       <ul className="flex flex-col gap-1">
-        <li className="cursor-pointer font-bold">README.md</li>
-        {headings.map((heading, ind) => (
-          <li
-            className={`flex cursor-pointer flex-row items-center justify-between gap-8 ${
-              heading.line === line
-                ? light
-                  ? "text-accentl"
-                  : "text-accent"
-                : light
-                ? "text-neutrall"
-                : "text-neutral"
-            }`}
-            key={ind}
-          >
-            <div>
-              {heading.heading.startsWith("# ")
-                ? heading.heading.replace("# ", "")
-                : heading.heading.startsWith("##")
-                ? heading.heading.replace("## ", "- ")
-                : heading.heading.replace("-- ", "-- ")}
-            </div>
-            <div>{heading.line}</div>
-          </li>
-        ))}
+        {files.length <= 0 ? (
+          <>
+            <li className="cursor-pointer font-bold">Splash</li>
+            {headings.map((heading, ind) => (
+              <li
+                className={`flex cursor-pointer flex-row items-center justify-between gap-8 ${
+                  heading.line === line
+                    ? light
+                      ? "text-accentl"
+                      : "text-accent"
+                    : light
+                    ? "text-neutrall"
+                    : "text-neutral"
+                }`}
+                key={ind}
+              >
+                <div>
+                  {heading.heading.startsWith("# ")
+                    ? heading.heading.replace("# ", "")
+                    : heading.heading.startsWith("##")
+                    ? heading.heading.replace("## ", "- ")
+                    : heading.heading.replace("-- ", "-- ")}
+                </div>
+                <div>{heading.line}</div>
+              </li>
+            ))}
+          </>
+        ) : (
+          files.map((file, ind) => (
+            <>
+              <li
+                className={`${
+                  ind === current
+                    ? light
+                      ? "text-accentl"
+                      : "text-accent"
+                    : light
+                    ? "text-neutrall"
+                    : "text-neutral"
+                } cursor-pointer font-bold`}
+                key={ind}
+                onClick={() => setCurrent(ind)}
+              >
+                {file}
+              </li>
+              {ind === current
+                ? headings.map((heading, ind) => (
+                    <li
+                      className={`flex cursor-pointer flex-row items-center justify-between gap-8 ${
+                        heading.line === line
+                          ? light
+                            ? "text-accentl"
+                            : "text-accent"
+                          : light
+                          ? "text-neutrall"
+                          : "text-neutral"
+                      }`}
+                      key={ind}
+                    >
+                      <div>
+                        {heading.heading.startsWith("# ")
+                          ? heading.heading.replace("# ", "")
+                          : heading.heading.startsWith("##")
+                          ? heading.heading.replace("## ", "- ")
+                          : heading.heading.replace("-- ", "-- ")}
+                      </div>
+                      <div>{heading.line}</div>
+                    </li>
+                  ))
+                : null}
+            </>
+          ))
+        )}
       </ul>
     </div>
   );
@@ -40,6 +99,9 @@ interface Props {
   headings: { heading: string; line: number }[];
   line: number;
   light: boolean;
+  files: string[];
+  current: number;
+  setCurrent: Dispatch<SetStateAction<number>>
 }
 
 export default Sidebar;
